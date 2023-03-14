d860052a09a19875b586d84964d3e536e3c0ea17
Mark <mrkswrn@gmail.com>
2023-03-14 23:20:56 +0100

    ramfetch: init at 1.1.0

---
+++ pkgs/tools/misc/ramfetch/default.nix
@@ -0,0 +1,33 @@
+{ lib
+, stdenv
+, fetchgit
+}:
+
+stdenv.mkDerivation rec {
+  name = "ramfetch";
+  version = "1.1.0";
+
+  src = fetchgit {
+    url = "https://codeberg.org/o69mar/ramfetch.git";
+    rev = "v${version}";
+    hash = "sha256-XUph+rTbw5LXWRq+OSKl0EjFac+MQAx3NBu4rWdWR3w=";
+  };
+
+  dontBuild = true;
+
+  installPhase = ''
+    runHook preInstall
+
+    install -D ramfetch $out/bin/ramfetch
+
+    runHook postInstall
+  '';
+
+  meta = with lib; {
+    description = "Tool which displays memory info";
+    homepage = "https://codeberg.org/o69mar/ramfetch";
+    platforms = platforms.linux;
+    license = licenses.mit;
+    maintainers = [ maintainers.markbeep ];
+  };
+}
